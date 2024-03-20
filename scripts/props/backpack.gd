164a67ec360876e88e86413fb39f9ac4975d2098
Noah <nordqvist.gaming@gmail.com>
2024-03-20 17:24:13 +0100

    add sfx and collect item trigger to backpack

---
+++ scripts/props/backpack.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+signal key_collected
+
+
+func _ready() -> void:
+	$Area3D.body_entered.connect(on_body_entered)
+
+
+func on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		$AudioStreamPlayer.play()
+		key_collected.emit()
